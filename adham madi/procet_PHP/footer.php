da7a1d3b16f5e9e168bf6070a38e9737b1761c52
adhmmadi21 <137719584+adhmmadi21@users.noreply.github.com>
2023-06-25 21:01:22 +0300

    Add files via upload

---
+++ adham madi/procet_PHP/footer.php
@@ -0,0 +1,34 @@
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="shop.php">shop</a>
+         <a href="contact.php">contact</a>
+         <a href="cart.php">cart</a>
+         <a href="orders.php">orders</a>
+      </div>
+
+
+      <div class="box">
+         <h3>contact info</h3>
+         <p> <i class="fas fa-phone"></i> +972 597 580 151 </p>
+         <p> <i class="fas fa-phone"></i> 082136701 </p>
+         <p> <i class="fas fa-envelope"></i> muhannedprof@gmail.com </p>
+         <p> <i class="fas fa-map-marker-alt"></i> Gaza , Rafah  </p>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+   </div>
+
+   <p class="credit">  copyright  @ <?php echo date('y'); ?> by <span>Prof</span> </p>
+
+</section>
